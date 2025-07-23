57e03e2feb231a045c728d0835f76b784930f1e5
Sparshagarwal29 <sparshagarwal2909@gmail.com>
2025-07-23 19:21:51 +0530

    duddli.py

--- removeDuplicate.py
+++ removeDuplicate.py
@@ -8,3 +8,23 @@ def check(nums):
 
 print(check(nums=[3,5,6,8,9,10,20]))
 print(check(nums=[1,2,5,3,6,8,9,11,13]))
+
+# we have to keep the unique elment a first then the space left we can keep any element of our chice , we have to do all this in place meaning in the same inped arr 
+# we have to return the number of unique number 
+def dubli(nums):
+    n = len(nums)
+    if n ==1:
+        return 1
+    i = 0 
+    j = i+1
+    while j < n:
+        if nums[j] != nums[i]:
+            i +=1
+            nums[i],nums[j] = nums[j] , nums[i]
+        j +=1
+    print(nums)        
+    return i+1                    
+
+print(dubli(nums=[1,1,1,1,1,2,2,2,2,3,3,4,4,5,5,6,6,7,7,8,9,9]))
+            
+    
