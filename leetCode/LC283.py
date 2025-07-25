35104a2cb681fcc7cf1eaa90780eb59e7e5f00a5
Sparshagarwal29 <sparshagarwal2909@gmail.com>
2025-07-25 17:44:59 +0530

    move zero

---
+++ leetCode/LC283.py
@@ -0,0 +1,51 @@
+# brute force soln on how to moke zero to the left side wityhout changing the order of the zon zero element 
+# t(n) == o(N) and s(n)  is also o(N)
+def moveZero(nums):
+    temp =[]
+    n = len(nums)
+    for i in range(n):
+        if nums[i]!= 0:
+            temp.append(nums[i])
+    nz = len(temp)        
+    for i in range(0,len(temp)):    
+        nums[i] = temp[i]   
+    for i in range(nz,n):
+        nums[i] = 0  
+    return nums
+nums=[1,0,2,4,3,0,0,3,5,1]
+print(moveZero(nums))
+
+
+# optimal soln -->
+# method -1 
+def leftZero(nums):
+    non_zero_pos = 0
+    
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[non_zero_pos], nums[i] = nums[i], nums[non_zero_pos]
+            non_zero_pos += 1
+    
+    return nums
+nums = [1, 0, 2, 4, 3, 0, 0, 3, 5, 1]
+print(leftZero(nums))
+# method 2 
+def zeroShift(nums):
+    i =0
+    if len(nums)== 1:
+        return
+    while i < len(nums):
+        if nums[i] == 0:
+            break
+        i +=1
+    if i == len(nums):
+            return
+    j = i+1
+    while j < len(nums):
+        if nums[j] != 0:
+            nums[i],nums[j] = nums[j],nums[i]
+            i +=1
+        j +=1
+    return nums
+print(zeroShift(nums=[1, 0, 2, 4, 3, 0, 0, 3, 5, 1]))
+
