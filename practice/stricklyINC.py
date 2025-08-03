280c317ae067468ae8b98141ef2fd80de0a2719d
Sparshagarwal29 <sparshagarwal2909@gmail.com>
2025-08-03 11:27:07 +0530

    contestQuestion

---
+++ practice/stricklyINC.py
@@ -0,0 +1,22 @@
+# a given array should be first strictly increasing then strictly deceasing then again structly increasing eg --[1,4,9,8,2,11,12]
+
+def check(nums):
+    i = 1
+    n = len(nums)
+    if nums[0] > nums[i]:
+        return False
+    if n < 4:
+        return False
+    while i < n and nums[i] > nums[i+1]:
+        i +=1
+    if i== 1 or i == n:
+        return False
+    while i < n and nums[i] < nums[i+1]:
+        i +=1
+    if i== 1 or i == n:
+        return False
+    while i < n and nums[i] > nums[i+1]:
+        i +=1
+    if i== 1:
+        return False
+    return i == n
