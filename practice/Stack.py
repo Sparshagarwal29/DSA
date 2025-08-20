0c66eda3a3fffd27f9405e5db89f0af0fae3c6cb
Sparshagarwal29 <sparshagarwal2909@gmail.com>
2025-08-20 22:31:03 +0530

    Stack

---
+++ practice/Stack.py
@@ -0,0 +1,22 @@
+class Stack:
+    def __init__(self):
+        self.iteam = []
+    def is_empty(self):
+        return (len(self.iteam) == 0)
+    def push(self,val):
+        self.iteam.append(val)
+    def pop(self):
+        if len(self.iteam) == 0 :
+            return "empty Stack"
+        else:
+            x = self.iteam.pop()
+            return x
+    def top(self):
+        if len(self.iteam) == 0 :
+            return "empty Stack"
+        else:
+            return self.iteam[-1]
+    def size(self):
+        return len(self.iteam)
+
+Stacke = Stack()  
